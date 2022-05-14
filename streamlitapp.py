50e3e5ce67ac93f379f89d55283302d5c4d67419
RyXiao <104197199+RyXiao@users.noreply.github.com>
2022-05-14 08:41:11 +0800

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,9 +1,13 @@
 import streamlit as st
 st.write(
     """
-# 📊 A/B Testing App
-Upload your experiment results to see the significance of your A/B test.
+# Shanghai COVID Curve
+Upload your Shanghai COVID with data and cases to see the curve.
 """
 )
 
 uploaded_file = st.file_uploader("Upload CSV", type=".csv")
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    st.line_chart(df)
