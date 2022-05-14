e9b0e86d691284fa395522e68b86823aca7d5da8
RyXiao <104197199+RyXiao@users.noreply.github.com>
2022-05-14 09:32:07 +0800

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -7,7 +7,7 @@ Upload your Shanghai COVID with data and cases to see the curve.
 """
 )
 
-uploaded_file = st.file_uploader("Upload CSV", type=".csv")
+uploaded_file = st.file_uploader("Upload EXCEL XLSX FILE", type=".XLSX")
 
 use_example_file = st.checkbox(
     "Use example file", False, help="Use in-built example file to demo the app"
@@ -17,10 +17,9 @@ use_example_file = st.checkbox(
 # If CSV is not uploaded and checkbox is filled, use values from the example file
 # and pass them down to the next if block
 if use_example_file:
-    uploaded_file = "Shanghai_COVID.csv"
-
+    uploaded_file = "Shanghai_COVID.XLSX"
 
 
 if uploaded_file:
-    df = pd.read_csv(uploaded_file)
+    df = pd.read_excel(uploaded_file)
     st.line_chart(df)
