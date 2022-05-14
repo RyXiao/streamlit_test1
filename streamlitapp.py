7c48acbf90962fc20b3b70a9cfffabedc9cb2ebe
RyXiao <104197199+RyXiao@users.noreply.github.com>
2022-05-14 09:19:28 +0800

    Update streamlitapp.py
    Add a default version files if there is no files uploaded.

--- streamlitapp.py
+++ streamlitapp.py
@@ -9,6 +9,18 @@ Upload your Shanghai COVID with data and cases to see the curve.
 
 uploaded_file = st.file_uploader("Upload CSV", type=".csv")
 
+use_example_file = st.checkbox(
+    "Use example file", False, help="Use in-built example file to demo the app"
+)
+
+
+# If CSV is not uploaded and checkbox is filled, use values from the example file
+# and pass them down to the next if block
+if use_example_file:
+    uploaded_file = "Shanghai_COVID.csv"
+
+
+
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
     st.line_chart(df)
